d7b985ccf242aad7c4f8bcd55385a724b45bc18b
James <canning.jimmy@gmail.com>
2016-10-08 15:12:16 -0700

    Full refactor after I realised the first attempt was not "using the connections between cities"

--- cdn.py
+++ cdn.py
@@ -4,7 +4,7 @@ class CDN(object):
     def __init__(self):
         """Initialize a CDN"""
         # This graph is a dictionary with server as keys and a list of conections (arcs) as values
-        self.graph = { 
+        self.graph = {
             'New York': ['London', 'Seattle'],
             'Chicago': ['Washington D.C.', 'Austin', 'Buenos Ares'],
             'San Francisco': ['Washington D.C.', 'Montreal'],
@@ -14,7 +14,7 @@ class CDN(object):
             'Montreal': ['San Francisco', 'Mexico City'],
             'Mexico City': ['Seattle'],
 
-            'London': ['Cairo', 'Moscow', 'Rome'],
+            'London': ['Cairo', 'Rome'],
             'Paris': ['Beijing', 'Moscow'],
             'Moscow': ['London', 'Berlin'],
             'Berlin': ['Paris', 'Berlin'],
@@ -26,9 +26,9 @@ class CDN(object):
             'Tokyo': ['Chicago', 'Mumbai'],
             'Beijing': ['Tokyo', 'Sydney'],
             'Bangkok': ['Beijing', 'Sydney'],
-            'Mumbai': ['Bangkok', 'Sao Paulo'],
-            'Sydney': ['Bangkok'],
-            'Seoul': ['Mumbai'],
+            'Mumbai': ['Bangkok', 'Sao Paulo', 'Seoul'],
+            'Sydney': ['Bangkok', 'Seoul'],
+            'Seoul': ['Mumbai', 'Tokyo'],
 
             'Buenos Ares': ['Moscow', 'Sao Paulo'],
             'Sao Paulo': ['Johannesburg', 'Buenos Ares']}
@@ -38,64 +38,76 @@ class CDN(object):
         for server in list(self.graph.keys()):
             self.content[server] = []
 
-    def put(self, file, server):
-        """puts content on every server on the network
+    def post(self, file, user_server):
+        """posts content to every server on the network
 
         first argument 'file' is the content to upload.
-        Second argument 'server' is ther users server"""
-        for lsts in list(self.content.values()):
-            lsts.append(file)
-        return 'Status:200, the file named "{}" was succesfuly distributed from {}.'\
-            .format(file, server)
+        Second argument 'user_server' is ther users server they are posing from"""
+        self.update_network(user_server, 'store_data', file)
+        return (200, 'Distributed {}'.format(file))
 
-    def get(self, file, server):
-        """gets content from the network and returns the path it took
+    def get(self, file, user_server):
+        """gets content from the network and shows the path used
 
         first argument 'file' is the content to get
         second argument ' user_server' is where we will get it from"""
-        if file in self.content[server]:
-            return 'status:200, retrieved file named "{}" from {}'.format(file, server)
+        seen = []
+        potential_paths = [[user_server, ]]
+        while potential_paths:
+            path = potential_paths[0]
+            potential_paths = potential_paths[1:]
+            source = path[0]
+            if file in self.content[source]:
+                return (200, 'Retrieved {} via {}'.format(file, path))
+            for i in self.graph.get(source, []):
+                if i not in seen:
+                    potential_paths.append([i, ] + path)
+                    seen.append(i)
+        return (404, '{} does not exist'.format(file))
+
+    def put(self, file, user_server):
+        """replace content on servers
+
+        first argument 'file' is the content to replace
+        second argument 'user_server' is where we start"""
+        if file in self.content[user_server]:
+            self.update_network(user_server, 'update_data', file)
+            return (200, 'Updated {}'.format(file))
         else:
-            return 'status 404, there is no file named "{}" on {} server'.format(file, server)
+            return (404, '{} does not exist'.format(file))
 
-    def post(self, old_file, new_file):
-        """update content on servers
-
-        first argument 'old_file' is the content to replace
-        second argument 'new_file' is the content to replace is with"""
-        if old_file in self.content['Tokyo']:
-            for lst in self.content.values():
-                lst.remove(old_file)
-                lst.append(new_file)
-            return 'status:200, file named "{}" has been updated to file named "{}"'\
-                .format(old_file, new_file)
-        else:
-            return 'status 404 there is no file named "{}"'.format(old_file)
-
-    def delete(self, file):
+    def delete(self, file, user_server):
         """remove content
 
-        first argument is the content to be removed"""
-        if file in self.content['Tokyo']:
-            for lst in self.content.values():
-                lst.remove(file)
-            return 'status:200, file named "{}" has been deleted'.format(file)
+        first argument 'file' is the content to be removed
+        second argument 'user_server' is where we start"""
+        if file in self.content[user_server]:
+            self.update_network(user_server, 'remove_data', file)
+            return (200, 'Delete {}'.format(file))
         else:
-            return 'status 404 there is no file named "{}"'.format(file)
+            return (404, '{} does not exist'.format(file))
 
-    def find_path(self, start, end, path=[]):
-        """Finds a path between start and end nodes in a graph"""
-        path = path + [start]
-        if start == end:
-            return path
-        if start not in self.graph:
-            return None
-        for node in self.graph[start]:
-            if node not in path:
-                newpath = self.find_path(node, end, path)
-                if newpath:
-                    return newpath
-        return None
+    def update_network(self, start_node, action, file):
+        """stores, updates or removes data from the network depending on 'action' argument"""
+        seen = [start_node]
+        nodes = [start_node]
+        while nodes:
+            node = nodes.pop()
+            if action == 'store_data':
+                if node not in self.content:
+                    self.content[node] = []
+                self.content[node].append(file)
+            if action == 'update_data':
+                if node not in self.content:
+                    self.content[node] = []
+                    self.content[node].append(file)
+            if action == 'remove_data':
+                if node in self.content:
+                    self.content[node].remove(file)
+            for connected_node in self.graph.get(node, []):
+                if connected_node not in seen:
+                    seen.append(connected_node)
+                    nodes.append(connected_node)
 
 if __name__ == "__main__":
     cdn = CDN()
@@ -103,9 +115,9 @@ if __name__ == "__main__":
     print(cdn.content)
     print("")
     print('puting verious files on from various servers')
-    print(cdn.put('vid', 'Tokyo'))
-    print(cdn.put('book', 'London'))
-    print(cdn.put('photo', 'Cairo'))
+    print(cdn.post('vid', 'Tokyo'))
+    print(cdn.post('book', 'London'))
+    print(cdn.post('photo', 'Cairo'))
     print("")
     print('new state of cdn')
     print(cdn.content)
@@ -114,25 +126,19 @@ if __name__ == "__main__":
     print(cdn.get('vid', 'Tokyo'))
     print("")
     print('try to get file named "doc" from Tokyo server')
-    print(cdn.get('scan', 'Tokyo'))
+    print(cdn.get('doc', 'Tokyo'))
     print("")
-    print('update file named "book" to file named "Awsome book!"')
-    print(cdn.post('book', 'Awsome book!'))
+    print('update file named "book" from Rome')
+    print(cdn.put('book', 'Rome'))
     print("")
-    print('new state of cdn')
-    print(cdn.content)
+    print('try to update file named "doc" from Rome"')
+    print(cdn.put('doc', 'Rome'))
     print("")
-    print('try to update file named "doc" to file named "Awsome doc!"')
-    print(cdn.post('doc', 'Awsome doc!'))
-    print("")
-    print('delete file named "vid"')
-    print(cdn.delete('vid'))
+    print('delete file named "vid" from London')
+    print(cdn.delete('vid', 'London'))
     print("")
     print('new state of cdn')
     print(cdn.content)
     print("")
-    print('try to delete file named "scan"')
-    print(cdn.delete('scan'))
-    print("")
-    print('find path from Sydney to Seattle')
-    print(cdn.find_path('Sydney', 'Seattle'))
+    print('try to delete file named "scan" from Cairo')
+    print(cdn.delete('scan', 'Cairo'))
